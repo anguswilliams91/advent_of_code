2cc9f87c3e860a56a3f1871bbc8ad12eaccc3859
Angus Williams <anguswilliams91@gmail.com>
2021-12-01 20:14:29 +0000

    day 1 part 2

--- day_01/solution.py
+++ day_01/solution.py
@@ -11,12 +11,16 @@ def load_input(path: Path) -> Sequence[int]:
     return depths
 
 
-def count_number_of_depth_increases(depths: Sequence[int]) -> int:
+def count_number_of_depth_increases(depths: Sequence[int], window_size: int = 1) -> int:
     """Counts the number of times depth increases in a sequence of sonar measurements."""
-    depth_changes = [x - y for x, y in zip(depths[1:], depths[:-1])]
+    depth_changes = [
+        sum(depths[i : i + window_size]) - sum(depths[i - 1 : i - 1 + window_size])
+        for i in range(1, len(depths) - window_size + 1)
+    ]
     return sum(d > 0 for d in depth_changes)
 
 
 if __name__ == "__main__":
     depths = load_input(Path("input.txt"))
-    print(count_number_of_depth_increases(depths))
+    print(f"Part one: {count_number_of_depth_increases(depths, window_size=1)}")
+    print(f"Part two: {count_number_of_depth_increases(depths, window_size=3)}")
--- day_01/test_solution.py
+++ day_01/test_solution.py
@@ -1,9 +1,16 @@
 """Tests for solution."""
+
+import pytest
+
 from day_01.solution import count_number_of_depth_increases
 
 
 _EXAMPLE_INPUT = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 
-def test_given_example_answer_is_recovered():
-    assert count_number_of_depth_increases(_EXAMPLE_INPUT) == 7
+@pytest.mark.parametrize("window_size,expected", [(1, 7), (3, 5)])
+def test_given_example_answer_is_recovered(window_size, expected):
+    assert (
+        count_number_of_depth_increases(_EXAMPLE_INPUT, window_size=window_size)
+        == expected
+    )
