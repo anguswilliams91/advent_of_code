7a016a6ca13b119ea1de3eda54815d364c3beb28
Angus Williams <anguswilliams91@gmail.com>
2021-12-03 20:49:38 +0000

    day 3 parts 1 and 2

--- day_01/test_solution.py
+++ day_01/test_solution.py
@@ -9,7 +9,7 @@ _EXAMPLE_INPUT = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 
 @pytest.mark.parametrize("window_size,expected", [(1, 7), (3, 5)])
-def test_given_example_answer_is_recovered(window_size, expected):
+def test_example_solution_is_recovered(window_size, expected):
     assert (
         count_number_of_depth_increases(_EXAMPLE_INPUT, window_size=window_size)
         == expected
