8671a9950811a7989c86ea3b200b773edf04593b
Angus Williams <anguswilliams91@gmail.com>
2021-12-09 22:18:57 +0000

    tidy day 9

--- day_09/solution.py
+++ day_09/solution.py
@@ -1,7 +1,7 @@
 """9. Smoke basin."""
 
 from functools import reduce
-from typing import List, Sequence, Tuple
+from typing import Sequence, Set, Tuple
 
 import numpy as np
 
@@ -9,6 +9,7 @@ HeightMap = np.ndarray
 
 
 def get_neighbours(r: int, c: map, height_map: HeightMap) -> Sequence[Tuple[int, int]]:
+    """Finds the neighbours of a point in a 2D rectangular grid."""
     length, breadth = height_map.shape
     neighbours = []
     if r > 0:
@@ -43,7 +44,7 @@ def parse_map_desription(height_map_description: str) -> HeightMap:
     return np.array(height_map)
 
 
-def find_low_points(height_map: HeightMap) -> List[int]:
+def find_low_points(height_map: HeightMap) -> Sequence[int]:
     """Finds the low points and sums them."""
     low_points = []
     length, breadth = height_map.shape
@@ -56,34 +57,34 @@ def find_low_points(height_map: HeightMap) -> List[int]:
 
 
 def sum_of_low_points(height_map_description: str) -> int:
+    """Sums the risks of the low points in a height map."""
     height_map = parse_map_desription(height_map_description)
     return sum(height_map[p] + 1 for p in find_low_points(height_map))
 
 
-def find_basin_sizes(height_map_description: str) -> Sequence[int]:
+def find_points_in_basin(
+    point: Tuple[int, int], height_map: HeightMap
+) -> Set[Tuple[int, int]]:
+    """Finds the points in a basin that contains the given point."""
+    r, c = point
+    neighbours = get_neighbours(r, c, height_map)
+    points_in_basin = {point}
+    for neighbour in neighbours:
+        if height_map[neighbour] > height_map[point] and height_map[neighbour] != 9:
+            points_in_basin = points_in_basin | find_points_in_basin(
+                neighbour, height_map
+            )
+    return points_in_basin
+
+
+def product_of_biggest_basins(height_map_description: str) -> int:
     height_map = parse_map_desription(height_map_description)
     low_points = find_low_points(height_map)
     basin_sizes = []
 
-    def find_points_in_basin(point, height_map):
-        r, c = point
-        neighbours = get_neighbours(r, c, height_map)
-        points_in_basin = {point}
-        for neighbour in neighbours:
-            if height_map[neighbour] > height_map[point] and height_map[neighbour] != 9:
-                points_in_basin = points_in_basin | find_points_in_basin(
-                    neighbour, height_map
-                )
-        return points_in_basin
-
     for low_point in low_points:
         basin_sizes.append(len(find_points_in_basin(low_point, height_map)))
 
-    return basin_sizes
-
-
-def product_of_biggest_basins(height_map_description: str) -> int:
-    basin_sizes = find_basin_sizes(height_map_description)
     return reduce(lambda x, y: x * y, list(reversed(sorted(basin_sizes)))[:3])
 
 
