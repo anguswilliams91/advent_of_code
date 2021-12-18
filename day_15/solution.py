ee32450ab1749672d5e225803266f195cf170172
Angus Williams <anguswilliams91@gmail.com>
2021-12-18 21:11:37 +0000

    day 15

--- day_15/solution.py
+++ day_15/solution.py
@@ -1,9 +1,9 @@
 """15. Chiton."""
 
+from heapq import heappop, heappush
 from typing import Sequence, Tuple
 
 import numpy as np
-
 
 RiskMap = np.ndarray
 
@@ -33,28 +33,54 @@ def get_neighbours(
     return neighbours
 
 
-def find_risk_of_path_with_lowest_total_risk(risk_map_description: str) -> int:
-    """Finds the risk of the path with the lowest total risk."""
-    risk_map = parse_risk_map(risk_map_description)
+def find_risk_of_path_with_lowest_total_risk(
+    risk_map_description: str, use_full_map: bool
+) -> int:
+    """Uses Djikstra to find the risk of the points in the path with lowest total risk."""
+    risk_map = (
+        make_full_map(risk_map_description)
+        if use_full_map
+        else parse_risk_map(risk_map_description)
+    )
     length, breadth = risk_map.shape
-    unvisited_points = {(i, j) for i in range(length) for j in range(breadth)}
     tentative_risk = np.ones_like(risk_map) * np.inf
     tentative_risk[0, 0] = 0
-    current_point = (0, 0)
-    while (length - 1, breadth - 1) in unvisited_points:
+    priority_queue = [(0, (0, 0))]
+    while priority_queue:
+        _, current_point = heappop(priority_queue)
         for neighbour in get_neighbours(current_point, risk_map):
-            if neighbour in unvisited_points:
-                candidate_risk = tentative_risk[current_point] + risk_map[neighbour]
-                if candidate_risk < tentative_risk[neighbour]:
-                    tentative_risk[neighbour] = candidate_risk
-        unvisited_points.remove(current_point)
-        current_point = sorted(unvisited_points, key=tentative_risk.__getitem__)[0]
+            candidate_risk = tentative_risk[current_point] + risk_map[neighbour]
+            if candidate_risk < tentative_risk[neighbour]:
+                tentative_risk[neighbour] = candidate_risk
+                heappush(priority_queue, (candidate_risk, neighbour))
+
     return tentative_risk[(length - 1, breadth - 1)]
+
+
+def make_full_map(risk_map_description: str) -> RiskMap:
+    """Makes the full map from the input by tiling it and adding risk."""
+    base_map = parse_risk_map(risk_map_description)
+    height, width = base_map.shape
+    full_map = np.zeros((5 * height, 5 * width))
+    for i in range(5):
+        for j in range(5):
+            full_map[
+                height * i : height * (i + 1), width * j : width * (j + 1)
+            ] = base_map + (i + j)
+    full_map = full_map % 9
+    full_map[full_map == 0] = 9
+    return full_map
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         risk_map_description = f.read()
 
-    part_one = find_risk_of_path_with_lowest_total_risk(risk_map_description)
+    part_one = find_risk_of_path_with_lowest_total_risk(
+        risk_map_description, use_full_map=False
+    )
+    part_two = find_risk_of_path_with_lowest_total_risk(
+        risk_map_description, use_full_map=True
+    )
     print(f"Part one: {part_one}")
+    print(f"Part one: {part_two}")
--- day_15/test_solution.py
+++ day_15/test_solution.py
@@ -1,5 +1,7 @@
 """Tests for day 15."""
 
+import pytest
+
 from day_15.solution import find_risk_of_path_with_lowest_total_risk
 
 _EXAMPLE_INPUT = """1163751742
@@ -14,5 +16,11 @@ _EXAMPLE_INPUT = """1163751742
 2311944581"""
 
 
-def test_example_solution_is_recovered():
-    assert find_risk_of_path_with_lowest_total_risk(_EXAMPLE_INPUT) == 40
+@pytest.mark.parametrize("use_full_map,expected", [(False, 40), (True, 315)])
+def test_example_solution_is_recovered(use_full_map, expected):
+    assert (
+        find_risk_of_path_with_lowest_total_risk(
+            _EXAMPLE_INPUT, use_full_map=use_full_map
+        )
+        == expected
+    )
