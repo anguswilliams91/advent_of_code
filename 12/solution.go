8a486ef83dbd359686fa5db8aa4a3b7742e7d5fb
Angus Williams <anguswilliams91@gmail.com>
2022-12-12 17:42:52 +0000

    day 12

---
+++ 12/solution.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"aoc"
+	"image"
+	"strings"
+)
+
+type heightMap struct {
+	height    map[image.Point]rune
+	start     image.Point
+	end       image.Point
+	altStarts []image.Point
+}
+
+type location struct {
+	coords   image.Point
+	distance int
+}
+
+type queue []location
+
+func (q *queue) pop() location {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+
+func (q *queue) push(p location) {
+	*q = append(*q, p)
+}
+
+func newHeightMap() *heightMap {
+	r := heightMap{}
+	r.height = make(map[image.Point]rune)
+	r.altStarts = []image.Point{}
+	return &r
+}
+
+func parseInput(input string) *heightMap {
+	hm := newHeightMap()
+	for r, s := range strings.Split(input, "\n") {
+		for c, v := range []rune(s) {
+			p := image.Point{r, c}
+			hm.height[p] = v
+			if h := string(v); h == "S" {
+				hm.height[p] = 'a'
+				hm.start = p
+			} else if h == "E" {
+				hm.height[p] = 'z'
+				hm.end = p
+			} else if h == "a" {
+				hm.altStarts = append(hm.altStarts, p)
+			}
+		}
+	}
+	return hm
+}
+
+func findShortestRoute(hm *heightMap, fromAnywhere bool) int {
+	visited := make(map[image.Point]bool)
+	q := queue{location{coords: hm.start, distance: 0}}
+	visited[hm.start] = true
+	if fromAnywhere {
+		for _, l := range hm.altStarts {
+			q.push(location{coords: l, distance: 0})
+			visited[l] = true
+		}
+	}
+	for len(q) > 0 {
+		p := q.pop()
+		if p.coords.Eq(hm.end) {
+			return p.distance
+		}
+		h0 := hm.height[p.coords]
+		for _, delta := range []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+			n := p.coords.Add(delta)
+			if h, ok := hm.height[n]; ok {
+				if h-h0 <= 1 && !visited[n] {
+					visited[n] = true
+					q.push(location{coords: n, distance: p.distance + 1})
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func solve(input string) aoc.Solution[int, int] {
+	hm := parseInput(input)
+	return aoc.Solution[int, int]{
+		PartOne: findShortestRoute(hm, false),
+		PartTwo: findShortestRoute(hm, true),
+	}
+}
+
+func main() {
+	puzzleInput := aoc.LoadInput("input.txt")
+	timedSolve := aoc.Timer("Day twelve", solve)
+	timedSolve(puzzleInput)
+}
